aec008dfb12ca639c69b6fd14d933bb91dd1e7e1
Bence Racskó <bracsko94@gmail.com>
2026-01-02 21:57:31 +0100

    fucking test

--- systemthemedetector/src/test/java/hu/benzor/systemthemedetector/internal/environment/EnvironmentDetectorTest.java
+++ systemthemedetector/src/test/java/hu/benzor/systemthemedetector/internal/environment/EnvironmentDetectorTest.java
@@ -1,16 +1,19 @@
 package hu.benzor.systemthemedetector.internal.environment;
 
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import hu.benzor.systemthemedetector.api.environment.DesktopEnvironment;
 import hu.benzor.systemthemedetector.api.environment.Platform;
 import hu.benzor.systemthemedetector.internal.environment.propertyreader.PropertyReader;
 
@@ -21,59 +24,48 @@ public class EnvironmentDetectorTest {
     PropertyReader systemPropertyReader;
 
     @Mock
-    PropertyReader environmentPropertyReader;
+    PropertyReader environmentVariableReader;
 
-    @InjectMocks
     EnvironmentDetector environmentDetector;
+
+    @BeforeEach
+    void setUp() {
+        environmentDetector = new EnvironmentDetector(systemPropertyReader, environmentVariableReader);
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        {
+            "Linux, LINUX",
+            "Windows 11, WINDOWS",
+            "MacOS, MACOS",
+            "asdasd, UNKNOWN",
+            "null, UNKNOWN"
+        }
+    )
+    void testGetPlatform(String input, Platform expected) {
+        when(systemPropertyReader.getValue(eq("os.name")))
+            .thenReturn("null".equals(input) ? Optional.empty() : Optional.of(input));
+        
+        Assertions.assertEquals(expected, environmentDetector.getPlatform());
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        {
+            "Gnome, GNOME",
+            "KDE, KDE",
+            "XFCE, XFCE",
+            "X-Cinnamon, CINNAMON",
+            "MATE, MATE",
+            "asdasd, UNKNOWN",
+            "null, UNKNOWN"
+        }
+    )
+    void testGetDesktop(String input, DesktopEnvironment expected) {
+        when(environmentVariableReader.getValue(eq("XDG_CURRENT_DESKTOP")))
+            .thenReturn("null".equals(input) ? Optional.empty() : Optional.of(input));
+        Assertions.assertEquals(expected, environmentDetector.getDesktop());
+    }
     
-    @Test
-    void testGetPlatformLinux() {
-
-        when(systemPropertyReader.getValue("os.name")).thenReturn(Optional.of("Linux"));
-
-        Platform result = environmentDetector.getPlatform();
-
-        Assertions.assertEquals(Platform.LINUX, result);
-
-    }
-
-    @Test
-    void testGetPlatformWindows() {
-
-        when(systemPropertyReader.getValue("os.name")).thenReturn(Optional.of("Windows 11"));
-
-        Platform result = environmentDetector.getPlatform();
-
-        Assertions.assertEquals(Platform.WINDOWS, result);
-    }
-
-    @Test
-    void testGetPlatformMac() {
-
-        when(systemPropertyReader.getValue("os.name")).thenReturn(Optional.of("MacOs"));
-
-        Platform result = environmentDetector.getPlatform();
-
-        Assertions.assertEquals(Platform.MACOS, result);
-    }
-
-    @Test
-    void testGetPlatformEmpty() {
-
-        when(systemPropertyReader.getValue("os.name")).thenReturn(Optional.empty());
-
-        Platform result = environmentDetector.getPlatform();
-
-        Assertions.assertEquals(Platform.UNKNOWN, result);
-    }
-
-    @Test
-    void testGetPlatformGibberish() {
-
-        when(systemPropertyReader.getValue("os.name")).thenReturn(Optional.of("asdaasd"));
-
-        Platform result = environmentDetector.getPlatform();
-
-        Assertions.assertEquals(Platform.UNKNOWN, result);
-    }
 }
